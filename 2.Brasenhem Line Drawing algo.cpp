4e9f63ee415668064e4a8b2ca256d9b49462a34e
bayazid28 <86534024+bayazid28@users.noreply.github.com>
2021-12-04 21:37:33 +0600

    Update 2.Brasenhem Line Drawing algo.cpp

--- 2.Brasenhem Line Drawing algo.cpp
+++ 2.Brasenhem Line Drawing algo.cpp
@@ -4,19 +4,20 @@
 
 int main()
 {
-int gd=DETECT, gm, x1, y1, x2, y2;
+int gd=DETECT, gm, x1, y1, x2, y2,dx,dy,p;
 printf("Enter the first and second point where x1<x2: ");
 scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
 
 initgraph(&gd, &gm, (char*)"");
-int dx, dy, p;
 
 dx=abs(x2-x1);
 dy=abs(y2-y1);
+
 p=2*dy - dx;
 
 while(x1<=x2){
 putpixel(x1,y1,WHITE);
+delay(50);
 if(p>=0){
 y1=y1+1;
 p=p+2*dy-2*dx;
@@ -25,8 +26,6 @@ else{
 p=p+2*dy;
 }
 x1=x1+1;
-delay(50);
-
 }
 getch();
 closegraph();
